78f47267ac2d6d996e2e851c047207722e8f28ea
kiranqureshi1 <kiranqureshi@outlook.com>
2019-01-15 16:36:45 +0000

    12th methods added

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -39,3 +39,9 @@ def number_to_full_month_name(month_number)
     return "September"
   end
 end
+
+def number_to_short_month_name(month_number)
+  if month_number == 1
+    return "Jan"
+  end
+end 
